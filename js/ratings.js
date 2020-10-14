ceed432f55c79bd2ade07747e532dd1762cfd672
Friday Ameh Matthew <48910588+ameh01@users.noreply.github.com>
2020-10-14 15:13:18 +0100

    first commit

---
+++ js/ratings.js
@@ -0,0 +1,21 @@
+function collect_ratings() {
+
+    ratings {
+        count = 0;
+        sum = 0;
+        average = 0;
+    }
+
+    let rating = 0;
+    
+}
+
+document.querySelectorAll() = rating;
+
+let elements = [];
+elements.forEach(myFunction);
+
+function myfunction(element) {
+    
+    
+}
